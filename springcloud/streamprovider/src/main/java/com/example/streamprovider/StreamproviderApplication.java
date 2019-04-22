169fccd28dfe98cad94aad1a78230dcd1143a80f
蒲茂超 <1442169607@qq.com>
2019-04-22 18:35:34 +0800

    增加cloudstream

---
+++ springcloud/streamprovider/src/main/java/com/example/streamprovider/StreamproviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamproviderApplication.class, args);
+	}
+
+}
