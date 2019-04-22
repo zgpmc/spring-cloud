943531c15c34fae19533f81c1861ad7c58a36bd5
蒲茂超 <1442169607@qq.com>
2019-04-22 15:37:36 +0800

    update

--- springcloud/configclient2/src/main/java/com/example/configclient2/ctrl/Test.java
+++ springcloud/configclient2/src/main/java/com/example/configclient2/ctrl/Test.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RefreshScope//使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
-@RequestMapping(value = "test/")
+@RequestMapping(value = "/test")
 public class Test
 {
     @Value("${hello}")
